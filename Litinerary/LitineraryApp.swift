03d416f6294894b6ba30bc54f4842e98dd5b955e
Alina Brandizzi <alinabrandizzi@gmail.com>
2020-10-22 16:06:38 +0200

    Initial Commit

---
+++ Litinerary/LitineraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LitineraryApp.swift
+//  Litinerary
+//
+//  Created by Alina Brandizzi on 22/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct LitineraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
