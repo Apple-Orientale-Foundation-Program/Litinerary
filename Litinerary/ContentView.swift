5a9d0ec6e4b4189fb8fdc8c4d1bc11792d9ba557
GiusyFerrigno <giusyferrigno@rocketmail.com>
2020-10-22 16:25:07 +0200

    First View Bozza

--- Litinerary/ContentView.swift
+++ Litinerary/ContentView.swift
@@ -9,9 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello Smart Cookies!")
-            .padding()
-            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+        Home()
     }
 }
 
@@ -19,4 +17,45 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Home: View{
+    
+    @State var data = [
+        Card(id: 0, image:"toledo", title : "Itinerario Toledo", details: "Questo itinerario parla di blah blah blah blah blah",expand: false),
+        Card(id: 1, image:"piazzadante", title: "Napoli Geniale", details: "Questo itinerario parla di blah blaah", expand: false),
+        Card(id:2, image: "posillipo", title: "Posillipo Itinerario", details:"Itinerario", expand: false)
+    ]
+    
+    var body: some View{
+        VStack{
+            
+            ScrollView(.vertical, showsIndicators: false) {
+                
+                VStack(spacing:15){
+                    
+                    ForEach(self.data){i in
+                        Image(i.image)
+                            .resizable()
+                            .frame(height: 250)
+                            .cornerRadius(60)
+                            .padding(.horizontal)
+                        
+                    }
+                    
+                    
+                }
+            
+        }
+    }
+}
+
+//Sample per i dati homeview
+struct Card : Identifiable{
+var id: Int
+var image: String
+var title: String
+var details: String
+var expand: Bool
+}
 }
