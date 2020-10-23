6be508446d1744f060a423ffe5b8ec033de2ff0b
Wanda Punzi Zarino <w.zarino@gmail.com>
2020-10-23 16:24:58 +0200

    app icon dimensione

--- Litinerary/SplashView.swift
+++ Litinerary/SplashView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct SplashView: View {
     var body: some View {
         VStack {
-            Image("AppIconSF")
+            Image("AppIcon2")
                 .resizable()
                 .scaledToFit()
-                .frame(width: 80)
+                .frame(width: 130)
             Image("Litinerary")
         }
     }
