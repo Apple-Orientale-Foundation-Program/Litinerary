9be18d9c387da0475a8c4b68d50b06598d577ab8
Wanda Punzi Zarino <w.zarino@gmail.com>
2020-10-23 16:29:44 +0200

    eliminato navigation view

--- Litinerary/SplashView.swift
+++ Litinerary/SplashView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct SplashView: View {
     var body: some View {
-        VStack {
-            Image("AppIcon2")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 130)
-                .padding()
-            Image("Litinerary")
-                        }
+            VStack {
+                Image("AppIcon2")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 130)
+                    .padding()
+                Image("Litinerary")
+            }
     }
 }
 
