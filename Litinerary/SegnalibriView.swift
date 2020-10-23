2044a7de80241fa60041ed42e797416dec33950c
Wanda Punzi Zarino <w.zarino@gmail.com>
2020-10-23 15:48:21 +0200

    Inserire funzione "segnalibri"

--- Litinerary/SegnalibriView.swift
+++ Litinerary/SegnalibriView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct SegnalibriView: View {
     var body: some View {
         NavigationView{
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("Inserire funzione per aggiungere a questa view gli itinerari preferiti")
                 .navigationTitle("Segnalibri")
         }
     }
