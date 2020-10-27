d8e112e02e28843747f37525bf3d59b137facf29
Sonia Di Nicoli <soniadinicoli@gmail.com>
2020-10-27 15:23:53 +0100

    SHADOW

--- Litinerary/ContentView.swift
+++ Litinerary/ContentView.swift
@@ -36,7 +36,7 @@ struct ContentView: View {
                         .frame(maxWidth: .infinity, minHeight: 100)
                         .cornerRadius(50)
                         .padding(.horizontal)
-                        .shadow(color: .pink, radius: 0.3, x:1.2, y:3.8)
+                        .shadow(color: .pink, radius: 0.3, x:0.0, y:3.8)
                       HStack(alignment: .bottom, spacing: 10.0) {
                         Image(systemName: "figure.walk")
                           .foregroundColor(.white)
