0f02b01effc77be4f5965c4249d8e10f99ede8a4
Alina Brandizzi <alinabrandizzi@gmail.com>
2020-10-28 14:24:09 +0100

    mappa fatta

--- Litinerary/MapView/RouteView.swift
+++ Litinerary/MapView/RouteView.swift
@@ -12,8 +12,8 @@ func initAnnotations()->[MKAnnotation]{
     map.removeAnnotations(map.annotations)
     let sourceCoordinate = CLLocationCoordinate2D(latitude: 40.844877, longitude: 14.257189)
     let twoCoordinate = CLLocationCoordinate2D(latitude: 40.8461097, longitude: 14.2541287)
-    let threeCoordinate = CLLocationCoordinate2D(latitude: 40.8461297, longitude: 14.2475626)
-    let fourCoordinate = CLLocationCoordinate2D(latitude: 40.849438, longitude: 14.251902)
+    let threeCoordinate = CLLocationCoordinate2D(latitude: 40.848586, longitude: 14.252477)
+    let fourCoordinate = CLLocationCoordinate2D(latitude: 40.849539, longitude: 14.250640)
     let fiveCoordinate = CLLocationCoordinate2D(latitude: 40.8399021, longitude: 14.2531124)
     let sixCoordinate = CLLocationCoordinate2D(latitude: 40.836984, longitude: 14.248613)
     let sevenCoordinate = CLLocationCoordinate2D(latitude: 40.8342492, longitude: 14.2418517)
@@ -65,16 +65,40 @@ func initAnnotations()->[MKAnnotation]{
     destinationPin.title = "Lungomare Caracciolo"
     map.addAnnotation(destinationPin)
     
+    //    req.twoPin = MKMapItem(placemark: MKPlacemark(coordinate: twoCoordinate))
+    //    req.threePin = MKMapItem(placemark: MKPlacemark(coordinate: threeCoordinate))
+    //    req.fourPin = MKMapItem(placemark: MKPlacemark(coordinate: fourCoordinate))
+    //    req.fivePin = MKMapItem(placemark: MKPlacemark(coordinate: fiveCoordinate))
+    //    req.sixPin = MKMapItem(placemark: MKPlacemark(coordinate: sixCoordinate))
+    //    req.sevenPin = MKMapItem(placemark: MKPlacemark(coordinate: sevenCoordinate))
+    
     let req = MKDirections.Request()
     req.source = MKMapItem(placemark: MKPlacemark(coordinate: sourceCoordinate))
-//    req.twoPin = MKMapItem(placemark: MKPlacemark(coordinate: twoCoordinate))
-//    req.threePin = MKMapItem(placemark: MKPlacemark(coordinate: threeCoordinate))
-//    req.fourPin = MKMapItem(placemark: MKPlacemark(coordinate: fourCoordinate))
-//    req.fivePin = MKMapItem(placemark: MKPlacemark(coordinate: fiveCoordinate))
-//    req.sixPin = MKMapItem(placemark: MKPlacemark(coordinate: sixCoordinate))
-//    req.sevenPin = MKMapItem(placemark: MKPlacemark(coordinate: sevenCoordinate))
     req.destination = MKMapItem(placemark: MKPlacemark(coordinate: destinationCoordinate))
     
+    let req2 = MKDirections.Request()
+    req2.source = MKMapItem(placemark: MKPlacemark(coordinate: twoCoordinate))
+    req2.destination = MKMapItem(placemark: MKPlacemark(coordinate: threeCoordinate))
+
+    let req3 = MKDirections.Request()
+    req3.source = MKMapItem(placemark: MKPlacemark(coordinate: threeCoordinate))
+    req3.destination = MKMapItem(placemark: MKPlacemark(coordinate: fourCoordinate))
+
+    let req4 = MKDirections.Request()
+    req4.source = MKMapItem(placemark: MKPlacemark(coordinate: fourCoordinate))
+    req4.destination = MKMapItem(placemark: MKPlacemark(coordinate: fiveCoordinate))
+
+    let req5 = MKDirections.Request()
+    req5.source = MKMapItem(placemark: MKPlacemark(coordinate: fiveCoordinate))
+    req5.destination = MKMapItem(placemark: MKPlacemark(coordinate: sixCoordinate))
+
+    let req6 = MKDirections.Request()
+    req6.source = MKMapItem(placemark: MKPlacemark(coordinate: sixCoordinate))
+    req6.destination = MKMapItem(placemark: MKPlacemark(coordinate: sevenCoordinate))
+
+    let req7 = MKDirections.Request()
+    req7.source = MKMapItem(placemark: MKPlacemark(coordinate: sevenCoordinate))
+    req7.destination = MKMapItem(placemark: MKPlacemark(coordinate: destinationCoordinate))
     
     let directions = MKDirections(request: req)
 
@@ -82,6 +106,9 @@ func initAnnotations()->[MKAnnotation]{
         if err != nil {
             print ((err?.localizedDescription)!)
             return
+            
+//            var transportType: MKDirectionsTransportType voglio mettere a piedi
+            
         }
         let polyline = direct?.routes.first?.polyline
         map.addOverlay(polyline!)
