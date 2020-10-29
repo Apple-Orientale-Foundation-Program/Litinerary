f24db3fbb17965d7d851933dd7ef4cd21c0ecd55
Sonia Di Nicoli <soniadinicoli@gmail.com>
2020-10-29 19:20:59 +0100

    bho

--- Data/Routes/StopContent.swift
+++ Data/Routes/StopContent.swift
@@ -23,7 +23,7 @@ func findStop(Stops: [Stop], title: String)->Stop?{
 
 var actualStops = AmicaGenialeStops
 
-//xcode must die
+
 var AmicaGenialeStops = [
     Stop(title: "Via Mezzocannone", description: "Lenù cerca di separarsi da Lila dopo essersi sentita umiliata da lei durante una festa a casa della professoressa Galiani. Decisa a voler comprare a sue spese i libri scolastici, inizia a lavorare in una libreria in via Mezzocannone. Il figlio della professoressa, colpito dai discorsi fatti da Elena alla festa, va a trovarla a lavoro.", content: "“Girovagavo per via Mezzocannone durante la pausa del pranzo quando mi sentii chiamare. Era Armando, stava andando a dare un esame. Scoprii che studiava medicina e l’esame era difficile ma, prima di dileguarsi verso san Domenico Maggiore, si intrattenne ugualmente con me riempiendomi di complimenti e riattaccando a parlare di politica. In serata si affacciò addirittura in libreria, aveva preso ventotto, era felice. Mi chiese il numero di telefono, dissi che non avevo telefono; mi chiese se facevamo una passeggiata la domenica successiva, gli dissi che la domenica dovevo aiutare mamma in casa. […] Insomma ero contenta perché gli ero evidentemente piaciuta e fui gentile, ma non disponibile. Le parole di Lila avevano comunque fatto danno. Mi sentivo mal vestita, mal pettinata, falsa nei toni, ignorante. […] Così Napoli, l’Italia, il mondo erano ridiventati velocemente una landa nebbiosa dentro cui non mi orientavo più. Armando parlava, io facevo sì con la testa, ma di ciò che diceva capivo poco o niente. “", description2: "", content2: "", audio: "agletturauno"),
     
