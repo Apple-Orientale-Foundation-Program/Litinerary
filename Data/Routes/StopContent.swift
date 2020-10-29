33cfd2dd695d4c64bfaa4e98411f900ddcb4ed40
Alina Brandizzi <alinabrandizzi@gmail.com>
2020-10-29 10:51:40 +0100

    asdasdasdasdsad

--- Data/Routes/StopContent.swift
+++ Data/Routes/StopContent.swift
@@ -11,35 +11,36 @@ import MapKit
 struct Stop {
     var title: String
     var description: String
+    var content: String
 }
 
 var AmicaGenialeStops = [
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla")
+    Stop(title: "Via Mezzocannone", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
 ]
 
 
 var AndreuccioStops = [
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla")
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
 ]
 
 
 var CaccioppoliStops = [
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla"),
-    Stop(title: "bla", description: "bla")
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
+    Stop(title: "bla", description: "bla", content: "bla"),
 ]
