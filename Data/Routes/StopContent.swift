cc5b9489f38e6f6f7f678844749a7c94248fbe25
Alina Brandizzi <alinabrandizzi@gmail.com>
2020-10-29 10:50:07 +0100

    miao

---
+++ Data/Routes/StopContent.swift
@@ -0,0 +1,45 @@
+//
+//  StopContent.swift
+//  Litinerary
+//
+//  Created by Alina Brandizzi on 29/10/2020.
+//
+
+import Foundation
+import MapKit
+
+struct Stop {
+    var title: String
+    var description: String
+}
+
+var AmicaGenialeStops = [
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla")
+]
+
+
+var AndreuccioStops = [
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla")
+]
+
+
+var CaccioppoliStops = [
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla"),
+    Stop(title: "bla", description: "bla")
+]
