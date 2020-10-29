de593e3cfc89f2bc80e2e128acdb9058b3e32903
Francesco Sacerdoti <francesco.sacerdoti@gmail.com>
2020-10-29 17:43:24 +0100

    manca detail

--- Litinerary/MapView/RouteView.swift
+++ Litinerary/MapView/RouteView.swift
@@ -8,66 +8,35 @@
 import SwiftUI
 import MapKit
 
-//Modal structure
-//struct RouteView: View {
-//    @State var modal = false
-//    var body: some View {
-//        VStack{
-//            Text("Read more")
-            
-//        }
-//        .onTapGesture {
-//            self.modal.toggle()
-//        }
-//        .sheet(isPresented: $modal) {
-//            ModalView()
-//        }
-//    }
-    
-func initAnnotations(itinerario: [Pin])->[MKAnnotation]{
-  let req = MKDirections.Request()
-    var oldPin = Pin(lat: 0.0, long: 0.0, title: "")
-  var index = 0
+let map = MKMapView()
 
-  map.removeOverlays(map.overlays)
-  map.removeAnnotations(map.annotations)
-  for pin in itinerario{
-    let aPin = MKPointAnnotation()
-    aPin.coordinate = pin.coord()
-    aPin.title = pin.title
-    map.addAnnotation(aPin)
+class MapViewModel: ObservableObject {
+  @Published var stop: Stop
 
-    if index > 0 {
-      req.source = MKMapItem(placemark: MKPlacemark(coordinate: oldPin.coord()))
-      req.destination = MKMapItem(placemark: MKPlacemark(coordinate: pin.coord()))
-      req.transportType = .walking
-      let directions = MKDirections(request: req)
-      directions.calculate { (direct, err) in
-        if err != nil {
-          print ((err?.localizedDescription)!)
-          return
-        }
-        let polyline = direct?.routes.first?.polyline
-        map.addOverlay(polyline!)
-      }
-    }
-    index += 1
-    oldPin = pin
+  init(stop: Stop) {
+    self.stop = stop
   }
-  map.scaleOnAllAnnotations(spanFactor: 1.4)
-  return map.annotations
 }
 
-let map = MKMapView()
+struct mapView2: View{
+  @State var itinerario = [Pin]()
+  @State var detail: Bool
+  @ObservedObject var viewModel: MapViewModel
+
+  var body: some View{
+    mapView(itinerario: itinerario, detail: $detail, viewModel: viewModel)
+      .sheet(isPresented: $detail) {
+        DetailView(stop: viewModel.stop)
+      }
+  }
+}
 
 struct mapView : UIViewRepresentable {
 
   //@State var annotations: [MKAnnotation]
   @State var itinerario: [Pin]
-
-  func makeCoordinator() -> mapView.Coordinator {
-    return mapView.Coordinator()
-  }
+  @Binding var detail: Bool
+  @ObservedObject var viewModel: MapViewModel
 
   func makeUIView(context: UIViewRepresentableContext<mapView>) -> MKMapView{
     _ = initAnnotations(itinerario: itinerario)
@@ -78,7 +47,16 @@ struct mapView : UIViewRepresentable {
   func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<mapView>) {
   }
 
+  func makeCoordinator() -> mapView.Coordinator {
+    return mapView.Coordinator(self)
+  }
+
   class Coordinator : NSObject, MKMapViewDelegate{
+    var parent: mapView
+
+    init(_ parent: mapView) {
+      self.parent = parent
+    }
 
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) ->
     MKOverlayRenderer {
@@ -88,7 +66,14 @@ struct mapView : UIViewRepresentable {
       return render
     }
 
-
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+      if let title = view.annotation?.title {
+        if let findstop = findStop(Stops: actualStops, title: title!){
+          parent.viewModel.stop = findstop
+          parent.detail = true
+        }
+      }
+    }
   }
 }
 
@@ -124,8 +109,44 @@ extension MKMapView{
   }
 }
 
+func initAnnotations(itinerario: [Pin])->[MKAnnotation]{
+  let req = MKDirections.Request()
+  var oldPin = Pin(lat: 0.0, long: 0.0, title: "")
+  var index = 0
+
+  map.removeOverlays(map.overlays)
+  map.removeAnnotations(map.annotations)
+  for pin in itinerario{
+    let aPin = MKPointAnnotation()
+    aPin.coordinate = pin.coord()
+    aPin.title = pin.title
+    map.addAnnotation(aPin)
+
+    if index > 0 {
+      req.source = MKMapItem(placemark: MKPlacemark(coordinate: oldPin.coord()))
+      req.destination = MKMapItem(placemark: MKPlacemark(coordinate: pin.coord()))
+      req.transportType = .walking
+      let directions = MKDirections(request: req)
+      directions.calculate { (direct, err) in
+        if err != nil {
+          print ((err?.localizedDescription)!)
+          return
+        }
+        let polyline = direct?.routes.first?.polyline
+        map.addOverlay(polyline!)
+      }
+    }
+    index += 1
+    oldPin = pin
+  }
+  map.scaleOnAllAnnotations(spanFactor: 1.4)
+  return map.annotations
+}
+
+
 struct RouteView_Previews: PreviewProvider {
   static var previews: some View {
-    mapView(itinerario: AmicaGeniale)
+    mapView2(itinerario: AmicaGeniale, detail: false, viewModel: MapViewModel(stop: Stop()))
   }
 }
+
